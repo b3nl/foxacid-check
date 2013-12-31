9c46f8104204d731181fb2665b49f0239c5fea86
WBL_b3nl <b.lange@wbl-konzept.de>
2013-12-31 17:29:06 +0100

    * express and other apis

--- index.js
+++ index.js
@@ -1,13 +1,19 @@
-var oHTTP = require('http'), oServer = oHTTP.createServer(function (oRequest, oResponse) {
+var oApp = require('express')();
+
+oApp.get('/', function(oRequest, oResponse) {
 	"use strict";
 
-	var oURL = require('url'), oParsed = oURL.parse(oRequest.url, true);
+	var sBody = 'Please use the API.';
+	oResponse.writeHead(200, {'Content-Length': Buffer.byteLength(sBody), 'Content-Type': 'text/plain'});
+	oResponse.end(sBody);
+});
 
-	oResponse.writeHead(200, {'Content-Type': 'text/plain'});
+oApp.get('/static/:url', function(oRequest, oResponse) {
+	"use strict";
 
-	if (oParsed.query.url) {
-		oHTTP.get(
-			oParsed.query.url,
+	if (oRequest.params.url) {
+		require('http').get(
+			oRequest.params.url,
 			function (oSubResponse) {
 				var oBufferList = require('bl'), oHash = require('crypto').createHash('sha1');
 
@@ -15,14 +21,18 @@ var oHTTP = require('http'), oServer = oHTTP.createServer(function (oRequest, oR
 					if (!mError) {
 						oHash.update(oBuffer.toString());
 
-						oResponse.end(oHash.digest('hex'));
+						var sHash = oHash.digest('hex');
+
+						oResponse.writeHead(200, {'Content-Length': Buffer.byteLength(sHash), 'Content-Type': 'text/plain'});
+						oResponse.end(sHash);
 					} // if
 				}));
 			}
 		);
 	} else {
+		oResponse.writeHead(200, {'Content-Length': 1, 'Content-Type': 'text/plain'});
 		oResponse.end('0');
 	} // else
 });
 
-oServer.listen(Number(process.argv[2]));
+oApp.listen(Number(process.argv[2]));
